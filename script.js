00edf23aecc2ea59897e0d19915356af15cc1401
satyendra <skchandrawansi03@gmail.com>
2024-05-09 22:57:25 +0530

    initial commite

---
+++ script.js
@@ -0,0 +1,40 @@
+const showNavItem = () => {
+  const navitem = document.getElementById('navitem')
+  if (navitem.style.top === "0px") {
+    navitem.style.top = "-100%"
+    document.body.classList.remove("stop-scrolling");
+  }
+  else {
+
+    navitem.style.top = "0px"
+    document.body.classList.add("stop-scrolling");
+  }
+}
+
+const showLoginModal = () => {
+  const loginModal = document.getElementById('login-modal')
+  if (loginModal.style.display === "block") {
+    loginModal.style.display = "none"
+    document.body.classList.remove("stop-scrolling");
+  }
+  else {
+
+    loginModal.style.display = "block"
+    document.body.classList.add("stop-scrolling");
+  }
+
+}
+
+const showSignUpModal = () => {
+  const signUpModal = document.getElementById('signUp-modal')
+  if (signUpModal.style.display === "block") {
+    signUpModal.style.display = "none"
+    document.body.classList.remove("stop-scrolling");
+  }
+  else {
+
+    signUpModal.style.display = "block"
+    document.body.classList.add("stop-scrolling");
+  }
+}
+
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      boxShadow: {
+        'full': '0px 0px 15px 3px rgba(0,0,0,0.3)',
+      }
+    },
+  },
+  plugins: [],
+}
+
